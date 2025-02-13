696cb25e866d732eb09b0b3a88819e7ddfdd5376
Matias DEVEZE <matiasdeveze@gmail.com>
2025-02-13 08:24:25 +0100

    up

--- setup.sh
+++ setup.sh
@@ -7,15 +7,21 @@ sudo apt update -y
 
 # Ajout du dépôt officiel de Jenkins
 echo "[INFO] Ajout du dépôt Jenkins..."
-wget -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 # Mise à jour après l'ajout des dépôts
 sudo apt update -y
+sudo apt-get update -y
 
 # Installation des services nécessaires
 echo "[INFO] Installation de Java, Jenkins et MySQL..."
-sudo apt install -y openjdk-11-jdk jenkins mysql-server
+sudo apt install -y openjdk-11-jdk -y
+
+sudo apt-get install jenkins mysql-server -y
 
 # Installation de Gitea via Snap
 echo "[INFO] Installation de Gitea..."
